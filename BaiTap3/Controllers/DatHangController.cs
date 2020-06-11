0c1876a3af10ed2d81e0c6dd72381c4ea263c05d
theDragonbudd@gmail.com <theDragonbudd@gmail.com>
2020-06-11 17:36:16 +0700

    .

---
+++ BaiTap3/Controllers/DatHangController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap3.Controllers
+{
+    public class DatHangController : Controller
+    {
+        // GET: DatHang
+        public ActionResult Index()
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            ViewBag.List = new SelectList(new[]
+            {
+                new { ID="Volvo", Name="Volvo" },
+                new { ID="BMW", Name="BMW" },
+                new { ID="Ford", Name="Ford" },
+            }, "ID", "Name", 1);
+            return View();
+        }
+        public ActionResult ThemSP()
+        {
+
+        }
+
+    }
+}
---
+++ BaiTap3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BaiTap3.MvcApplication" Language="C#" %>
